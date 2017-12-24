357a9214d1325bf3a47b8469f762750f673344db
Michael Di Martino <34712549+dimo1207@users.noreply.github.com>
2017-12-24 07:51:05 -0500

    Version 1.03

--- nt_most_cmn.py
+++ nt_most_cmn.py
@@ -1,14 +1,13 @@
 """This module prints out tuples of the most common words
 found in the user-entered book. The first value in the tuple
 is the frequency of the word, followed by the word itself.
-Only words occurring 5 or more times are printed."""
+Only words occurring X or more times are printed."""
 
 
-print("Which book would you like to parse?")
-
-
-def nt_most_cmn(inpuT):
-    with open("New Testament/" + str(inpuT) + ".txt") as book:
+def nt_most_cmn(text):
+    if not text.istitle():
+        text = text.title()
+    with open("New Testament/" + text + ".txt") as book:
         chapters = [chp.split() for chp in book]
 
     count, words, chapter_list, chapter_tups = [], [], [], []
@@ -18,22 +17,28 @@ def nt_most_cmn(inpuT):
         chapter_count += 1
         chapter_list.append(chapter_count)
         # List comprehension strips words of punctuation/case and nests them in "words" list, organized by chapter:
-        words.append([word.strip("();:\"\'?!,.").lower() for word in chapter]) 
+        words.append([word.strip("();:\"\'?!,.").lower() for word in chapter])
 
     for chapter in words:
         # Creates a list of nested lists containing word lengths; corresponds to nested "words" lists:
         count.append([chapter.count(word) for word in chapter])
 
     for i in range(len(chapter_list)):
-        chapter_tups.append(sorted({(count, word) for count, word in zip(count[i], words[i])}, reverse=True))
+        chapter_tups.append(sorted(
+            # Set comprehension producing (int, str) tuples from count/words lists:
+            {(count, word) for count, word in zip(count[i], words[i])}, 
+            reverse=True)) # Sorts the final list of sets in descending order by count.
 
     # Test:
-    for i in range(len(chapter_tups)):
-        for l in range(len(chapter_tups[i])):
-            print(chapter_tups[i][l][1]) 
-    # chapter_tups can be indexed three times: the first indicates chapter, 
-    # the second indicates most common words tups (sorted by highest count first), and 
+    # for i in range(len(chapter_tups)):
+    #     for l in range(len(chapter_tups[i])):
+    #         print(chapter_tups[i][l][1])
+    # chapter_tups can be indexed three times: the first index indicates chapter,
+    # the second indicates most common words tups (sorted by highest count first), and
     # the last is indexed as 0 or 1, returing the count or the word (respectively).
 
+
 if __name__ == "__main__":
-    nt_most_cmn(input())
+    print("Which book would you like to parse?")
+    text = input()
+    nt_most_cmn(text)
