320dca1076e2854ab260a6c1a8d9ba59599c3da2
Michael Di Martino <34712549+dimo1207@users.noreply.github.com>
2017-12-31 13:55:37 -0500

    Version 1.01

--- nt_munge.py
+++ nt_munge.py
@@ -1,23 +1,29 @@
 from nt_most_cmn import nt_most_cmn
 
-chapter_tups = nt_most_cmn("John")
+chapter_tups = nt_most_cmn("Revelation")
 
 def munge():
+    hapaxes = []
+    x = -1  # Chapter index
     # Mask of unwanted words:
     unwanted = ['is', 'to', 'of', 'and', 'in', 'with', 'are', 'this',
                 'the', 'a', 'an', 'he', 'her', 'him', 'she', 'they', 'them', 'you',
                 'we', 'i', 'have', 'his', 'who', 'were', 'whose', 'when', 'was',
                 'said', 'from', 'whom', 'but', 'what', 'that', 'it', 'then', 'which',
-                'where', 'here']
+                'where', 'here', 'me', 'those', 'by', 'even', 'so', 'about', 'though',
+                'as', 'also']
 
-    for l in range(len(chapter_tups[0])):
-        while chapter_tups[0][l][1] in unwanted:
-            del chapter_tups[0][l]
-            chapter_tups[0].append("for deletion")
-        while chapter_tups[0][l][0] == 1:
-            del chapter_tups[0][l]
-            chapter_tups[0].append("for deletion")
+    for l in range(len(chapter_tups[x])):
+        while chapter_tups[x][l][1] in unwanted:
+            del chapter_tups[x][l]
+            chapter_tups[x].append("for deletion")
+        while chapter_tups[x][l][0] == 1:
+            hapaxes.append(chapter_tups[x][l])
+            del chapter_tups[x][l]
+            chapter_tups[x].append("for deletion")
 
-    del_idx = chapter_tups[0].index("for deletion")
-    return (chapter_tups[0][:del_idx])
+    del_idx = chapter_tups[x].index("for deletion")
+    print(chapter_tups[x][:del_idx])
+    # print(hapaxes)
+
 munge()
