f260d04580d98c2af3bb3d7d3a138ed815016f85
Michael Di Martino <34712549+dimo1207@users.noreply.github.com>
2018-01-05 14:25:07 -0500

    Version 1.00

--- nt_phrase_parse.py
+++ nt_phrase_parse.py
@@ -1,14 +1,22 @@
+"""This module loads a pickled dictionary containing the NRSV
+New Testament, and then searches each chapter of each book
+for the user-entered phrase or word.
+If found, the function returns the book title and
+the chapter in which the phrase/word was found."""
+
 import pickle
-nt_dict = pickle.load(open("nt_dict", "rb"))
 
 
-def nt_phrase_parse(phrase):
+def nt_phrase_parse():
+    """This is the sole function of the module."""
+    nt_dict = pickle.load(open("nt_dict", "rb"))
+    phrase = input("What word or phrase are you looking for?\n")
+
     for key in nt_dict.keys():
         for i in nt_dict[key]:
             if phrase in nt_dict[key][i]:
                 print(key, i)
 
 
-if __name__ == "__main__":
-    phrase = input("What word or phrase are you looking for?\n")
-    nt_phrase_parse(phrase)
+if __name__ == '__main__':
+    nt_phrase_parse()
