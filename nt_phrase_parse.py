3f60de16ebf07ef733ad17ca535acf75cd28fad7
Michael Di Martino <34712549+dimo1207@users.noreply.github.com>
2018-01-08 16:43:25 -0500

    Version 1.01

--- nt_phrase_parse.py
+++ nt_phrase_parse.py
@@ -11,11 +11,15 @@ def nt_phrase_parse():
     """This is the sole function of the module."""
     nt_dict = pickle.load(open("nt_dict", "rb"))
     phrase = input("What word or phrase are you looking for?\n")
+    total_count = 0
 
     for key in nt_dict.keys():
         for i in nt_dict[key]:
             if phrase in nt_dict[key][i]:
                 print(key, i)
+                total_count += nt_dict[key][i].count(phrase)
+
+    print("\nThis phrase occurs", total_count, "times in the New Testament.\n")
 
 
 if __name__ == '__main__':
