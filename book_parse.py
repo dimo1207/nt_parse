1d86660b780d0156a9b3b35135a3c1e10661baed
Michael Di Martino <34712549+dimo1207@users.noreply.github.com>
2018-01-12 10:30:44 -0500

    Version 1.10

--- book_parse.py
+++ book_parse.py
@@ -4,6 +4,7 @@ This version only finds the number of user-words per
 chapter, sites the chapter where the word occurs most
 frequently, and calculates the total occurence of this
 word in the book as a whole."""
+import pickle
 
 def book_parse():
     """This is the sole function of the module."""
@@ -13,36 +14,36 @@ def book_parse():
         text = text.title()
     print("What word would you like to find?")
     key_word = input()
+    nt_dict = pickle.load(open("nt_dict", "rb"))
 
-    with open("New Testament/" + text + ".txt") as book:
-        total_word_count = 0
-        chp_count = 0
-        max_chp = 0
-        words_in_chapter = []
-        for chapter in book:
-            chp_count += 1
-            word_count = 0
-            if key_word in chapter:
-                word_count += chapter.count(key_word)
-            words_in_chapter.append(word_count)
-            print("Occurence of \"{}\" in Chapter {}: ".format
-                  (key_word, chp_count), word_count)
-            total_word_count += word_count
-            most_freq = max(words_in_chapter)
-        for cnt in words_in_chapter:
-            if total_word_count == 0:
-                max_chp = "n/a"
-            elif words_in_chapter.count(most_freq) > 1:
-                max_chp = []
-                for i, word in enumerate(words_in_chapter):
-                    if word == most_freq:
-                        max_chp.append(i + 1)
-            elif cnt == max(words_in_chapter):
-                max_chp = (words_in_chapter.index(cnt) + 1)
-        print("\nTotal times that the word \"{}\" occurs in {}: "
-              .format(key_word, text), total_word_count)
-        print("\nChapter where \"{}\" occurs most frequently: ".format
-              (key_word), max_chp)
+    total_word_count = 0
+    chp_count = 0
+    max_chp = 0
+    words_in_chapter = []
+    for chapter in nt_dict[text]:
+        chp_count += 1
+        word_count = 0
+        if key_word in nt_dict[text][chapter]:
+            word_count += nt_dict[text][chapter].count(key_word)
+        words_in_chapter.append(word_count)
+        print("Occurence of \"{}\" in Chapter {}: ".format
+              (key_word, chp_count), word_count)
+        total_word_count += word_count
+        most_freq = max(words_in_chapter)
+    for cnt in words_in_chapter:
+        if total_word_count == 0:
+            max_chp = "n/a"
+        elif words_in_chapter.count(most_freq) > 1:
+            max_chp = []
+            for i, word in enumerate(words_in_chapter):
+                if word == most_freq:
+                    max_chp.append(i + 1)
+        elif cnt == max(words_in_chapter):
+            max_chp = (words_in_chapter.index(cnt) + 1)
+    print("\nTotal times that the word \"{}\" occurs in {}: "
+            .format(key_word, text), total_word_count)
+    print("\nChapter where \"{}\" occurs most frequently: ".format
+            (key_word), max_chp)
 
 if __name__ == "__main__":
     book_parse()
