e5a0df16e029127ecd553bb783beec33ef0754fb
Michael Di Martino <34712549+dimo1207@users.noreply.github.com>
2018-01-16 11:19:00 -0500

    Version 1.02

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ import pickle
 
 
 class BookListButton(ListItemButton):
+    """Imports object referenced in .kv file."""
     pass
 
 class BookListLabel(ListItemLabel):
+    """Imports object referenced in .kv file."""
     pass
 
 class CurrentBook(BoxLayout):
+    """Imports object referenced in .kv file."""
     pass
 
 class NTParse(BoxLayout):
+    """Primary class of the app; includes all major functions."""
 
     # Connects the value in the TextInput widget to these fields:
     book_name_text_input = ObjectProperty()
@@ -23,41 +27,66 @@ class NTParse(BoxLayout):
     chapter_text_input = ObjectProperty()
 
 
+    # Decorator:
+    def custom_context_manager(primary_function):
+        """Provides standard enter/exit requirements for the primary functions."""
+        def wrapper_function(self):
+            self.nt_dict = pickle.load(open("nt_dict", "rb")) # imports the raw data
+            self.output_list.adapter.data = [] # clears the "data" list
+            primary_function(self) # executes the primary function that's being decorated
+            self.output_list._trigger_reset_populate() # resets listview with updated "data" list
+            return
+        return wrapper_function # not closing parentheses makes the function "ready to execute"
+
+
+    # The following three functions handle text input and set it to different variables:
     def search_text(self):
+        """Returns the text found in the TextInput box that follows "Book Name:".
+        If no text is entered, "John" is returned as the default."""
         if self.book_name_text_input.text:
             text = self.book_name_text_input.text
             return text
         return "John"
 
     def search_phrase(self):
+        """Returns the text found in the TextInput box that follows "Word or Phrase:".
+        If no text is entered, "love" is returned as the default."""
         if self.word_phrase_text_input.text:
             phrase = self.word_phrase_text_input.text
             return phrase
         return "love"
 
     def search_chapter(self):
+        """Returns the text found in the TextInput box that follows "Chapter:".
+        If no text is entered, 1 is returned as the default."""
         if self.chapter_text_input.text:
             idx = self.chapter_text_input.text
             return idx
         return 1
 
+
+    # Primary functions of the app (three in total):
+    @custom_context_manager
     def parse(self):
-        self.output_list.adapter.data = []
-        nt_dict = pickle.load(open("nt_dict", "rb"))
+        """This method iterates through New Testament text to find
+        user-entered word, finds the number of user-words per
+        chapter, sites the chapter where the word occurs most
+        frequently, and calculates the total occurence of this
+        word in the book as a whole."""
         text = self.search_text()
         key_word = self.search_phrase()
         
-        # the following check keeps the app from crashing on bad input:
-        if text not in nt_dict.keys():
+        # The following check keeps the app from crashing on bad input:
+        if text not in self.nt_dict.keys():
             text = "John"
 
         total_word_count, chp_count, max_chp = 0, 0, 0
         parsed_text, words_in_chapter = [], []
-        for chapter in nt_dict[text]:
+        for chapter in self.nt_dict[text]:
             chp_count += 1
             word_count = 0
-            if key_word in nt_dict[text][chapter]:
-                word_count += nt_dict[text][chapter].count(key_word)
+            if key_word in self.nt_dict[text][chapter]:
+                word_count += self.nt_dict[text][chapter].count(key_word)
             words_in_chapter.append(word_count)
             parsed_text.append("Occurence of \"{}\" in {}, Chapter {}: ".format
                 (key_word, text, chp_count) + str(word_count))
@@ -76,60 +105,60 @@ class NTParse(BoxLayout):
         total_times = "Total times that the word \"{}\" occurs in {}: ".format(key_word, text) + str(total_word_count)
         most_freq_chap = "Chapter where \"{}\" occurs most frequently: ".format(key_word) + str(max_chp)
 
-        # Add the output to the ListView
+        # "Extends" data list with desired output:
         self.output_list.adapter.data.extend([total_times])
         self.output_list.adapter.data.extend([most_freq_chap])
         self.output_list.adapter.data.extend([str(x) for x in parsed_text])
 
-        # Reset the ListView
-        self.output_list._trigger_reset_populate()
 
-
+    @custom_context_manager
     def search(self):
-        self.output_list.adapter.data = []
-        nt_dict = pickle.load(open("nt_dict", "rb"))
+        """This method searches each chapter of each book
+        in nt_dict for the user-entered phrase or word.
+        If found, the function returns the book title and
+        the chapter in which the phrase/word was found."""
         phrase = self.search_phrase()
         total_count = 0
         searched_text = []
 
-        for key in nt_dict.keys():
-            for i in nt_dict[key]:
-                if phrase in nt_dict[key][i]:
+        for key in self.nt_dict.keys():
+            for i in self.nt_dict[key]:
+                if phrase in self.nt_dict[key][i]:
                     searched_text.append(key + " " + str(i))
-                    total_count += nt_dict[key][i].count(phrase)
+                    total_count += self.nt_dict[key][i].count(phrase)
 
         search_result = "'{}' occurs ".format(phrase[0].upper() + phrase[1:]) + str(total_count) + " times in the New Testament:"
 
-        # Add the output to the ListView
+        # "Extends" data list with desired output:
         self.output_list.adapter.data.extend([str(search_result)])
         self.output_list.adapter.data.extend([str(x) for x in searched_text])
 
-        # Reset the ListView
-        self.output_list._trigger_reset_populate()
 
-
+    @custom_context_manager
     def most_common(self):
-        self.output_list.adapter.data = []
-        nt_dict = pickle.load(open("nt_dict", "rb"))
+        """This method prints out tuples of the most common words
+        found in the user-entered book. The first value in the tuple
+        is the frequency of the word, followed by the word itself.
+        Only words occurring X or more times are printed."""
         text = self.search_text()
         idx = int(self.search_chapter()) - 1
 
-        # the following checks keep the app from crashing on bad input:
-        if text not in nt_dict.keys():
+        # The following checks keep the app from crashing on bad input:
+        if text not in self.nt_dict.keys():
             text = "John"
-        if idx not in nt_dict[text].keys():
+        if idx not in self.nt_dict[text].keys():
             idx = 0
 
         count, words, chapter_list, chapter_tups, hapaxes = [], [], [], [], []
         chapter_count = 0
 
-        for chapter in nt_dict[text]:
+        for chapter in self.nt_dict[text]:
             chapter_count += 1
             chapter_list.append(chapter_count)
             # List comprehension strips words of punctuation/case
             # and nests them in "words" list, organized by chapter:
             words.append([word.strip("();:\"\'?!,.-").lower()
-                          for word in nt_dict[text][chapter].split()])
+                          for word in self.nt_dict[text][chapter].split()])
 
         for chapter in words:
             # Creates a list of nested lists containing word lengths;
@@ -165,7 +194,7 @@ class NTParse(BoxLayout):
                 del hapaxes[i]
                 hapaxes.append("for deletion")
 
-        # the following del statements index "for deletion" in each list,
+        # The following del statements index "for deletion" in each list,
         # and then delete each value following that index via slicing:
         del chapter_tups[idx][chapter_tups[idx].index("for deletion"):]
         del hapaxes[hapaxes.index("for deletion"):]
@@ -173,37 +202,39 @@ class NTParse(BoxLayout):
         label_1 = "Most commonly occurring words in the book of {}, Chapter {}:".format(text, idx + 1)
         label_2 = "Words that occur only once:" 
 
-        # Add the output to the ListView
+        # "Extends" data list with desired output:
         self.output_list.adapter.data.extend([label_1])
         self.output_list.adapter.data.extend([str(x) for x in chapter_tups[idx]])
         self.output_list.adapter.data.extend([label_2])
         self.output_list.adapter.data.extend([str(x) for x in hapaxes[::-1]])
 
-        # Reset the ListView
-        self.output_list._trigger_reset_populate()
-
 
     def clear(self):
-
+        """Clears ListView contents and resets display."""
         self.output_list.adapter.data = []
         self.output_list._trigger_reset_populate()
 
-    def reset_app(self):
 
+    def reset_app(self):
+        """Resets the app to starting conditions."""
         self.clear_widgets()
         self.add_widget(NTParse())
 
-    def show_text(self, button_text):
 
+    def show_text(self, button_text):
+        """Launches CurrentBook widget after clearing any existing widgets."""
         self.clear_widgets()
         self.current_book = CurrentBook()
         self.add_widget(self.current_book)
     
-    def store_button_text(self, button_text):
 
+    def store_button_text(self, button_text):
+        """Stores all of the text displayed on the button being pressed."""
         self.button_text = button_text
 
     def return_desired_book(self):
+        """Parses the button_text stored on press.
+        The processed data is then passed to the "CurrentBook" widget as a label."""
         nt_dict = pickle.load(open("nt_dict", "rb"))
         for key in nt_dict.keys():
             if key in self.button_text:  # checks for key inclusion
@@ -241,6 +272,8 @@ class NTParse(BoxLayout):
                 return key
 
     def return_desired_chapter(self):
+        """Parses the button_text stored on press.
+        The processed data is then passed to the "CurrentBook" widget as a label."""
         # covers cases with two-digit word count returns (book parse only):
         if self.button_text[-4] == ":":
             # covers cases with one-digit chapters:
@@ -257,6 +290,8 @@ class NTParse(BoxLayout):
         return chapter
 
     def return_bible_text(self):
+        """Uses the data from button_text as keys in nt_dict.
+        Returns the Bible text specified by the processed button_text data."""
         nt_dict = pickle.load(open("nt_dict", "rb"))
         book = ""
         chapter = ""
@@ -279,11 +314,13 @@ class NTParse(BoxLayout):
             # covers full parse chapters only:
             chapter = int(self.button_text[-2:])
 
-
         outcome = nt_dict[book][chapter].split()
 
-        for start in range(0, len(outcome), 12):
-            yield " ".join(outcome[start:start + 12])
+        # The following generator is used to yield "outcome" contents to ListView labels.
+        # It limits the length of each label to 11 words
+        # in an effort to ensure that everything is displayed properly:
+        for start in range(0, len(outcome), 11):
+            yield " ".join(outcome[start:start + 11])
 
 
 
